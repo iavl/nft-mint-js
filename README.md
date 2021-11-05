a122c840b4ebc4390dfe96c2e928cc794443e4fc
albert <albert@rss3.io>
2021-11-05 18:24:01 +0800

    add source code

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# nft-mint-js
+# mint nft with web3
+
+nft contract on matic: ```0xb488da8b17123f9506c44c17e6d3e6ae9b511b47```
+
+
+install dependencies
+```
+npm install
+```
+
+edit env.json
+```
+{
+  "privateKey": "xxx"
+}
+```
+
+run
+```
+node mint.js
+```
---
+++ mint.js
@@ -0,0 +1,58 @@
+const Web3 = require('web3');
+
+const abi = require('./abi.json');
+
+const { privateKey } = require('./env.json');
+
+
+// Provider
+const providerRPC = {
+    development: 'http://localhost:9933',
+    polygonMainnet: 'https://polygon-rpc.com/',
+};
+
+const web3 = new Web3(providerRPC.polygonMainnet); //Change to correct network
+
+const contractAddress = '0xb488dA8b17123F9506C44C17E6d3E6aE9B511B47';
+const _value = ['0x760524E21377a92C8b5CD18293eeCfc56e9e4296','0x760524E21377a92C8b5CD18293eeCfc56e9e4296'];
+
+/*
+   -- Send Function --
+*/
+// Create Contract Instance
+const contractInstance = new web3.eth.Contract(abi, contractAddress);
+
+// Build Increment Tx
+const mintTx = contractInstance.methods.batchMint(_value);
+
+const mint = async () => {
+    console.log(
+        `Calling the batchMint function at contract address: ${contractAddress}`
+    );
+
+    // Sign Tx with PK
+    let txCall = {
+        to: contractAddress,
+        data: mintTx.encodeABI(),
+        // gas: await web3.eth.estimateGas(mintTx),
+        gas: 2208000,
+        gasPrice: '30000000000'
+    };
+
+    console.log("txCall:", txCall);
+
+    const signedTx = await web3.eth.accounts.signTransaction(
+        txCall,
+        privateKey
+    );
+
+    console.log("signedTx:", signedTx);
+
+    // Send Tx and Wait for Receipt
+    const createReceipt = await web3.eth.sendSignedTransaction(
+        signedTx.rawTransaction
+    );
+    console.log(`Tx successful with hash: ${createReceipt.transactionHash}`);
+};
+
+mint();
