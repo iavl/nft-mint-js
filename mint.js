85850df2f9e102cc966719e2734b8edd34ff77c6
Albert <albert@rss3.io>
2022-07-16 13:58:27 +0800

    update for csb

--- mint.js
+++ mint.js
@@ -6,11 +6,13 @@ const { airdropsAddress } = require('./airdrops.json');
 
 // Provider
 const providerRPC = {
-    development: 'http://157.245.132.76:8545',
+    development: 'https://csbrpc.kindjeff.com/',
     polygonMainnet: 'https://polygon-rpc.com/',
 };
 
-const web3 = new Web3(providerRPC.polygonMainnet); //Change to correct network
+
+
+const web3 = new Web3(providerRPC.development); //Change to correct network
 
 // Create Contract Instance
 const contractInstance = new web3.eth.Contract(abi, contractAddress);
@@ -23,7 +25,7 @@ const mint = async (_value) => {
     // console.log(`Calling the batchMint function at contract address: ${contractAddress}`);
 
     // Build Increment Tx
-    var mintTx = contractInstance.methods.batchMint(_value);
+    var mintTx = contractInstance.methods.migrate(_value);
 
     // Sign Tx with PK
     var txCall = {
@@ -31,41 +33,33 @@ const mint = async (_value) => {
         data: mintTx.encodeABI(),
         gas: await mintTx.estimateGas({from: from}),
         // gas: 2208000,
-        gasPrice: '30000000000'
+        gasPrice: '1000000000'
     };
 
     // console.log("txCall:", txCall);
-
+    var startTime = Date.now();
     var signedTx = await web3.eth.accounts.signTransaction(
         txCall,
         privateKey
     );
+    var duration = (Date.now() - startTime)/1000;
+    console.log(`SignedTx Duration: ${duration}`);
 
     // console.log("signedTx:", signedTx);
 
+    startTime = Date.now();
     // Send Tx and Wait for Receipt
     const createReceipt = await web3.eth.sendSignedTransaction(
         signedTx.rawTransaction
     );
+    duration = (Date.now() - startTime)/1000;
     console.log(`Tx successful with hash: ${createReceipt.transactionHash}`);
+    console.log(`SendTx Duration: ${duration}`);
 };
 
 
 const batchMint = async () => {
-    var step = 150
-    for (var i = 0; i < airdropsAddress.length; i+=step) {
-        console.log("start from ", i)
-        var addrs = []
-        var idx = Math.min(airdropsAddress.length, i+step)
-        for (var j = i; j < idx; j++) {
-            // console.log(airdropsAddress[j])
-            addrs.push(airdropsAddress[j])
-        }
-        await mint(addrs);
-        console.log("first address: ", airdropsAddress[i])
-        console.log("last address: ", airdropsAddress[idx-1])
-        await sleep(10000);
-    }
+    await mint({account:from, handle:"migrate-test1", uri:"ipfs://bafkreie2c6lxtz4junqqmfcw47wgxdglzazp37y75pwcsd4ewnzcne4ilu",toAddresses:["0x55F110395C844963b075674e2956eb414018a7a7","0x0fefeD77Bb715E96f1c35c1a4E0D349563d6f6c0"],linkType:"0x666f6c6c6f770000000000000000000000000000000000000000000000000000"});
 }
 
 
